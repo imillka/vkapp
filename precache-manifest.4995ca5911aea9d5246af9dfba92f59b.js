b514b5e63274d7e961629566e521fb00190e53b4
imillka <imillka@yandex.ru>
2019-09-19 10:21:14 +0400

    Updates

---
+++ precache-manifest.4995ca5911aea9d5246af9dfba92f59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vkapp/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "f80bdb2638f2a2c041f7",
+    "url": "/vkapp/static/js/runtime~main.f80bdb26.js"
+  },
+  {
+    "revision": "1c63057609daecf34895",
+    "url": "/vkapp/static/js/main.1c630576.chunk.js"
+  },
+  {
+    "revision": "70abc9cc735907864860",
+    "url": "/vkapp/static/js/2.70abc9cc.chunk.js"
+  },
+  {
+    "revision": "1c63057609daecf34895",
+    "url": "/vkapp/static/css/main.d158c42b.chunk.css"
+  },
+  {
+    "revision": "70abc9cc735907864860",
+    "url": "/vkapp/static/css/2.47a1ede8.chunk.css"
+  },
+  {
+    "revision": "4b981e16783db8f87b2f1e343493b5fc",
+    "url": "/vkapp/index.html"
+  }
+];
